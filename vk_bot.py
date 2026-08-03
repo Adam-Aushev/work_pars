950b6e15946ea4839e6b52cc1b2bd2235e283c4f
Adam Aushev <adamaushev@MacBook-Air-Adam.local>
2026-08-03 22:20:20 +0300

    send massage

--- vk_bot.py
+++ vk_bot.py
@@ -19,36 +19,37 @@ vk = vk_api.VkApi(token=token)
 # Работа с сообщениями
 longpoll = VkLongPoll(vk)
 
-# Основной цикл
-tor_list = []
-for event in longpoll.listen():
-    # Если пришло новое сообщение
-    if event.type == VkEventType.MESSAGE_NEW:
-        # Если оно имеет метку для меня( то есть бота)
-        if event.to_me:
-            # Сообщение от пользователя
-            request = event.text
-            # Каменная логика ответа
-            if request == "привет":
-                write_msg("Хай", event.user_id) # type: ignore
+if __name__ == "__main__":
+    # Основной цикл
+    tor_list = []
+    for event in longpoll.listen():
+        # Если пришло новое сообщение
+        if event.type == VkEventType.MESSAGE_NEW:
+            # Если оно имеет метку для меня( то есть бота)
+            if event.to_me:
+                # Сообщение от пользователя
+                request = event.text
+                # Каменная логика ответа
+                if request == "привет":
+                    write_msg("Хай", event.user_id) # type: ignore
 
-            elif 'https:' in request:
-                os.system(f'ssh   root@94.159.114.147  "echo {request} >> files/youtube_links.txt"')
-            elif 'torrent' in request.lower() or 'торрент' in request.lower():
-                key_word = request[request.strip().find(' '):]
-                tor_list = rutor(key_word)
-                if tor_list:
-                    tor_text = ''
-                    for each in tor_list[:10]:
-                        tor_text += f"{tor_list.index(each)}:... {each['size']} | seed - {each['seed']} ... \n{each['text']}\n"
-                    if len(tor_text) < 9000:
-                        write_msg(tor_text, event.user_id) # type: ignore
+                elif 'https:' in request:
+                    os.system(f'ssh   root@94.159.114.147  "echo {request} >> files/youtube_links.txt"')
+                elif 'torrent' in request.lower() or 'торрент' in request.lower():
+                    key_word = request[request.strip().find(' '):]
+                    tor_list = rutor(key_word)
+                    if tor_list:
+                        tor_text = ''
+                        for each in tor_list[:10]:
+                            tor_text += f"{tor_list.index(each)}:... {each['size']} | seed - {each['seed']} ... \n{each['text']}\n"
+                        if len(tor_text) < 9000:
+                            write_msg(tor_text, event.user_id) # type: ignore
+                        else:
+                            write_msg("Слишком много результатов", event.user_id) # type: ignore
                     else:
-                        write_msg("Слишком много результатов", event.user_id) # type: ignore
+                        write_msg('найдено 0', event.user_id) # type: ignore
+                elif request.isdigit() and tor_list:
+                    write_msg('Файл сохранен!', event.user_id) # type: ignore
+                    save_file(tor_list[int(request)]['link'], source='../torrent/', file_type='.torrent')
                 else:
-                    write_msg('найдено 0', event.user_id) # type: ignore
-            elif request.isdigit() and tor_list:
-                write_msg('Файл сохранен!', event.user_id) # type: ignore
-                save_file(tor_list[int(request)]['link'], source='../torrent/', file_type='.torrent')
-            else:
-                write_msg("Не поняла вашего ответа...", event.user_id) # type: ignore
+                    write_msg("Не поняла вашего ответа...", event.user_id) # type: ignore
